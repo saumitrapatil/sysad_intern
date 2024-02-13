87544572c765914b70b9d73407e51e6800d7ce82
Saumitra Patil <94760203+saumitrapatil@users.noreply.github.com>
2024-02-13 16:27:53 +0530

    Updated server-restart.sh

--- server-restart.sh
+++ server-restart.sh
@@ -2,8 +2,8 @@
 
 uwsgi_id=$(ps -e | grep uwsgi | awk '{print $1}')
 if [ -z "$uwsgi_id" ]; then
-    uwsgi --socket project.sock --module sysad_intern.wsgi & disown
+    uwsgi --socket project.sock --module sysad_intern.wsgi --daemonize /dev/null
 else
     kill $uwsgi_id
-    uwsgi --socket project.sock --module sysad_intern.wsgi & disown
+    uwsgi --socket project.sock --module sysad_intern.wsgi & disown --daemonize /dev/null
 fi
