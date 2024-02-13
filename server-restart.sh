53596bfe877edb03c0daac311065bc6fb47bc741
Saumitra Patil <saumitrapatil2703@gmail.com>
2024-02-13 16:55:29 +0530

    testing env

--- server-restart.sh
+++ server-restart.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-uwsgi_id=$(ps -e | grep uwsgi | awk '{print $1}')
-if [ -z "$uwsgi_id" ]; then
-    uwsgi --socket project.sock --module sysad_intern.wsgi --daemonize /dev/null
-else
-    kill $uwsgi_id
-    uwsgi --socket project.sock --module sysad_intern.wsgi --daemonize /dev/null
-fi
+# uwsgi_id=$(ps -e | grep uwsgi | awk '{print $1}')
+# if [ -z "$uwsgi_id" ]; then
+#     uwsgi --socket project.sock --module sysad_intern.wsgi --daemonize /dev/null
+# else
+#     kill $uwsgi_id
+#     uwsgi --socket project.sock --module sysad_intern.wsgi --daemonize /dev/null
+# fi
+
+echo $PATH
