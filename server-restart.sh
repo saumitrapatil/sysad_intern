21628aa552b2921ba551188ccf365c89fd84a3b3
Saumitra Patil <saumitrapatil2703@gmail.com>
2024-02-13 16:58:10 +0530

    testing env

--- server-restart.sh
+++ server-restart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/venv/bin/activate
+
 # uwsgi_id=$(ps -e | grep uwsgi | awk '{print $1}')
 # if [ -z "$uwsgi_id" ]; then
 #     uwsgi --socket project.sock --module sysad_intern.wsgi --daemonize /dev/null
