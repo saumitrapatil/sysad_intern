0e031896ed0dd55129ed229b0ff87d2b0e468aa4
Saumitra Patil <94760203+saumitrapatil@users.noreply.github.com>
2024-06-02 00:20:28 +0530

    Update settings.py

--- sysad_intern/settings.py
+++ sysad_intern/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nx5*j24dkwq8=p_ws5-%tqz36t*-bms7bn-sh53bpbo=2f0azq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.131.218.104']
 
 
 # Application definition
